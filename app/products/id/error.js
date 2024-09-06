20ba9baf16c13dd287669cc03474a076ee7cce8c
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-06 11:43:31 +0200

    Added default Error component to error.js file

--- app/products/id/error.js
+++ app/products/id/error.js
@@ -1,3 +1,10 @@
 'use client';
 
-export 
+export  default function Error() {
+    return (
+        <div>
+            <h2>Oops! something went wrong.</h2>
+            <p>We couldn't load the product you requested. Please try again later.</p>
+        </div>
+    );
+}
