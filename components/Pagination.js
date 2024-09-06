da596e3deb1a7252251c9a59ff5db976948d0dae
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-06 15:58:42 +0200

    Updated Pagination component with new CSS classes and styling for Previous and Next links

--- components/Pagination.js
+++ components/Pagination.js
@@ -7,12 +7,14 @@ export default function Pagination({ currentPage }) {
     const router = useRouter();
 
     return (
-        <div>
+        <div className="flex justify-center items-center space-x-4 my-8">
             {currentPage > 1 && (
-                <Link href={`/?page=${currentPage - 1 }`}>Previous</Link>
+                <Link href={`/?page=${currentPage - 1 }`} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    Previous</Link>
             )}
             <span>Page {currentPage}</span>
-            <Link href={`/?page=${currentPage +1 }`}>Next</Link>
+            <Link href={`/?page=${currentPage +1 }`} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Next</Link>
         </div>
     );
 }
