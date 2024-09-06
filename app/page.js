b57ea9029c5007cd5ec994c4a0200297c0eca2a0
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-06 10:23:46 +0200

    Added export default async function Home with pagination logic

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
 import ProductList from '../components/ProductList';
 import Pagination from '../components/Paginatioin';
 
+export default async function Home({ searchParams}) {
+  const page = Number(searchParams.page) || 1;
+  const limit = 20;
+  const skip = (page - 1) * limit;
+}
