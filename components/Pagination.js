1a8f5bcc5a85d7849054876719ec2c946b7f90be
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-06 13:23:16 +0200

    Added pagination links and current page display to Pagination component

--- components/Pagination.js
+++ components/Pagination.js
@@ -6,5 +6,13 @@ import { useRouter } from 'next/navigation';
 export default function Pagination({ currentPage }) {
     const router = useRouter();
 
-    
+    return (
+        <div>
+            {currentPage > 1 && (
+                <Link href={`/?page=${currentPage - 1 }`}>Previous</Link>
+            )}
+            <span>Page {currentPage}</span>
+            <Link href={`/?page=${currentPage +1 }`}>Next</Link>
+        </div>
+    );
 }
