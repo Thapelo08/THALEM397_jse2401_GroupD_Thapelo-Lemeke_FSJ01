c793ba37e0f894d868fcd129a76befb1c4083d73
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-06 13:14:11 +0200

    Add Pagination component with currentPage prop and router integration inside the pagination.js file

---
+++ components/Pagination.js
@@ -0,0 +1,10 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+export default function Pagination({ currentPage }) {
+    const router = useRouter();
+
+    
+}
