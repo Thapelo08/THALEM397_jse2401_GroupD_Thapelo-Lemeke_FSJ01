c93d77671cc3ce9873cc13053e8684ca40a0a817
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-06 10:32:03 +0200

    Added fetch call to retrieve products data in Home function

--- app/page.js
+++ app/page.js
@@ -5,4 +5,9 @@ export default async function Home({ searchParams}) {
   const page = Number(searchParams.page) || 1;
   const limit = 20;
   const skip = (page - 1) * limit;
+
+  const productsData = await fetch (`https://next-ecommerce-api.vercel.app/products?limit=${limit}&skip=${skip}`, { cache: 'no-store' });
+  const products = await productsData.json();
+
+  
 }
