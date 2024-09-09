ee21557d84bae50e34a4b4eefe83459494e6a182
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-09-09 16:31:18 +0200

    "Comment out entire ProductList function"

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,12 +1,14 @@
-export default function ProductList({ products }) {
-    return (
-        <div>
-            {products.map((product) => (
-                <div key={product.id}>
-                    <h2>{product.title}</h2>
-                    <p>Price: ${product.price}</p>
-                    </div>
-            ))}
-        </div>
-    );
-}
+// export default function ProductList({ products }) {
+
+//     console.log("products are", products)
+//     return (
+//         <div>
+//             {products.map((product) => (
+//                 <div key={product.id}>
+//                     <h2>{product.title}</h2>
+//                     <p>Price: ${product.price}</p>
+//                     </div>
+//             ))}
+//         </div>
+//     );
+// }
